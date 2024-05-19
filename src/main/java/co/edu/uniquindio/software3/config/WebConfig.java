abff61a661371d5aebdf642f6a962158168bbc7f
kevinmoralesc <kevina.moralesc@uqvirtual.edu.co>
2024-05-19 11:12:35 -0500

    commit 2

--- src/main/java/co/edu/uniquindio/software3/config/WebConfig.java
+++ src/main/java/co/edu/uniquindio/software3/config/WebConfig.java
@@ -25,7 +25,7 @@ public class WebConfig implements WebMvcConfigurer {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("http://localhost:5173");
+        config.addAllowedOrigin("https://react-fazt-test-production-d95c.up.railway.app");
         config.setAllowedHeaders(Arrays.asList(
                 HttpHeaders.AUTHORIZATION,
                 HttpHeaders.CONTENT_TYPE,
