8c6e970de96d171f1325a7665cf3c87dde1ac989
kevinmoralesc <kevina.moralesc@uqvirtual.edu.co>
2024-05-27 02:14:25 -0500

    actualizacion url front

--- src/main/java/co/edu/uniquindio/software3/config/WebConfig.java
+++ src/main/java/co/edu/uniquindio/software3/config/WebConfig.java
@@ -25,7 +25,7 @@ public class WebConfig implements WebMvcConfigurer {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("https://frontpruebahumo-production.up.railway.app/");
+        config.addAllowedOrigin("https://frongrupotrfinal-production.up.railway.app/");
         config.setAllowedHeaders(Arrays.asList(
                 HttpHeaders.AUTHORIZATION,
                 HttpHeaders.CONTENT_TYPE,
